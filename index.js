be590153ccea8c726830d5a9786595c9c25673a0
Ryan Florence <rpflorence@gmail.com>
2013-08-08 18:24:39 -0700

    I lied!

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-module.exports = function(obj, method, fn) {
+module.exports = function(o, m, f) {
+  return arguments.length == 3 ? punch(o, m, f) : punchObject(o, m);
+};
+
+function punch(obj, method, fn) {
   var old = obj[method];
   obj[method] = function() {
     var args = [].slice.call(arguments, 0);
     args.unshift(old.bind(obj));
     return fn.apply(obj, args);
   };
-};
+}
+
+function punchObject(obj, ext) {
+  for (var method in ext) {
+    if ('function' == typeof obj[method] && 'function' == typeof ext[method]) {
+      punch(obj, method, ext[method]);
+    } else {
+      obj[method] = ext[method];
+    }
+  }
+  return obj;
+}
 
--- test.js
+++ test.js
@@ -13,3 +13,22 @@ punch(baby, 'poo', function(old, oz) {
 var result = baby.poo(2);
 console.assert(result === 'pooped 2oz of really stinky stuff', result);
 
+var obj = {
+  x: 1,
+  sum: function(y) {
+    return this.x + y;
+  }
+};
+
+result = obj.sum(2);
+console.assert(result === 3, result);
+
+punch(obj, {
+  sum: function(old, y) {
+    return old(y) + y;
+  }
+});
+
+result = obj.sum(2);
+console.assert(result === 5, result);
+
